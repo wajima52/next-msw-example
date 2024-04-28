b6ad14e6ca4676f1f45685ebccfac6bf65c37f44
wajima <mikkywaji@icloud.com>
2024-04-28 16:10:41 +0900

    メニューリストを追加

--- src/app/_components/organizms/TaskManagementBlock.tsx
+++ src/app/_components/organizms/TaskManagementBlock.tsx
@@ -1,8 +1,41 @@
+import React from "react";
+import Link from "next/link";
 
 export const TaskManagementBlock = () => {
+  const links  = [
+    {
+      href: "/",
+      label: "検索"
+    },
+    {
+      href: "/",
+      label: "インボックス"
+    },
+    {
+      href: "/",
+      label: "今日"
+    },
+    {
+      href: "/",
+      label: "近日予定"
+    },
+    {
+      href: "/",
+      label: "フィルター&ラベル"
+    }
+  ]
   return (
     <div className={"py-3"}>
-      <h1 className={"text-2xl font-bold"}>Task Management</h1>
+      <div className={"font-bold"}>タスク管理</div>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index} className={"my-1 hover:bg-red-200 hover:text-red-600"}>
+            <Link href={link.href}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
